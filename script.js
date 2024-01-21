ca11aa53ff97555bbdb28d3ea480070c01c8e985
you-are-el <you_are_el@protonmail.com>
2024-01-21 15:24:00 +0100

    reduce files sizes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentWindow = 1;
-const totalWindows = 15; // Update this as per the number of windows you have
+const totalWindows = 14;
 const base64Icon = 'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAANlBMVEUAAAD/////mQDMzMyZmZlmZmb4+Pjn59bTfwDAwMC3fwCtqZCGhoYz/2YAgAD/+/BVVVX///+oh7OkAAAAEnRSTlP//////////////////////wDiv78SAAAACXBIWXMAAAsTAAALEwEAmpwYAAABGElEQVQ4ja2TzW6EMAwGx3YSqESl5v0fclcCStmE0gNLlg0c6xNoBvvLDxJ5VqIuD+AKT1pxu8WDkNK6vPMwc+iQEvLOdeOlQ/vj33hJVDKQTyErgQfAuk1qS6Aq+spadajXJlIZrhLW02r+u4PU/Dyiej+M0LZmlbBc8pdQzwIY6IrwfcbJ427R7e65PC7vI3p/IeDyK8NFgnW7AHuGgMwZAihsl6mdj4JA4XLckV14fo5uj8DXxiQC/TlBuyyYPaxRoFefzWfzWZtsPluTWywEI9jdQa82NTo1OgU/Nm5sdOyWBcBUUHq1qdOp06mzsdOx0+nAEWfnAB/zL6AqDNFdnqICqDBE3OUPIwrIQAT3ecGfZxcB/gDW1FLDgGIW7AAAAABJRU5ErkJggg=='; // Replace with your actual base64 string
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,39 +16,27 @@ function setVH() {
 }
 
 window.addEventListener('resize', setVH);
-setVH(); // Call the function to set the initial value
+setVH();
 
 
 function nextWindow() {
     if (currentWindow < totalWindows) {
-        // Hide the current window
         document.getElementById(`window${currentWindow}`).classList.add('hidden');
-
-        // Move to the next window
         currentWindow++;
-
-        // Show the next window
         document.getElementById(`window${currentWindow}`).classList.remove('hidden');
     }
 }
 
 function previousWindow() {
     if (currentWindow > 1) {
-        // Hide the current window
         document.getElementById(`window${currentWindow}`).classList.add('hidden');
-
-        // Move to the previous window
         currentWindow--;
-
-        // Show the previous window
         document.getElementById(`window${currentWindow}`).classList.remove('hidden');
     } else {
-        // Handle the case when there are no more previous windows
         alert("This is the first step of the OP_CAT tutorial.");
     }
 }
 
-// Function to close the error pop-up
 function closeErrorPopup(popID) {
     var errorPopup = document.getElementById(popID);
     errorPopup.style.display = 'none';
@@ -59,23 +47,18 @@ class BitcoinStack {
         this.container = document.getElementById(containerId);
         this.stack = arr;
         this.updateStackDisplay();
-        this.opcodeCounter = 0; // Initialize the opcode counter
+        this.opcodeCounter = 0;
         this.counterDisabled = false;
     }
         
     updateCounter() {  
-        if (currentWindow == 10) {
-            // Decrement the counter
+        if (currentWindow == 9) {
             if (this.counterDisabled == false) {
                 this.opcodeCounter++;
             }
-
-            // Update the display
             document.getElementById('opcode-counter').textContent = this.opcodeCounter;
 
-            // Check if the counter has reached 0
             if (this.opcodeCounter >= 200) {
-                // Show error-popup
                 var errorPopup = document.getElementById("error-popup-200");
                 errorPopup.style.display = 'block';
                 this.counterDisabled = true;
@@ -106,15 +89,12 @@ class BitcoinStack {
             const topItem = this.stack.pop();
             const nextItem = this.stack.pop();
             const concatenatedItem = nextItem + topItem;
-            
-            // Calculate the byte size of the concatenated item
             const concatenatedItemSize = new Blob([concatenatedItem]).size;
             
             if (concatenatedItemSize <= limit) {
                 this.stack.push(concatenatedItem);
                 this.updateCounter();
             } else {
-                // Show error-popup
                 if (limit == 520) {
                     var errorPopup = document.getElementById(errorPopupId);
                     errorPopup.style.display = 'block';
@@ -122,7 +102,6 @@ class BitcoinStack {
                     var errorPopup = document.getElementById(errorPopupId);
                     errorPopup.style.display = 'block';
                 }
-                // Put the items back on the stack
                 this.stack.push(nextItem);
                 this.stack.push(topItem);
             }
@@ -142,32 +121,22 @@ class BitcoinStack {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const stack1 = new BitcoinStack('bitcoin-stack-1', ['abc', '123']); // Stack in window 1 with 2 initial items
-    const stack2 = new BitcoinStack('bitcoin-stack-2', ['X']); // Stack in window 2 with 1 initial items
-    const stack3 = new BitcoinStack('bitcoin-stack-3', ['Item']); // Stack in window 2 with 1 initial items
-    const stack4 = new BitcoinStack('bitcoin-stack-4', ['Item']); // Stack in window 2 with 1 initial items
-    const stack5 = new BitcoinStack('bitcoin-stack-5', ['Item']); // Stack in window 2 with 1 initial items
-    const stack6 = new BitcoinStack('bitcoin-stack-6', ['Item']); // Stack in window 2 with 1 initial items
+    const stack1 = new BitcoinStack('bitcoin-stack-1', ['abc', '123']);
+    const stack2 = new BitcoinStack('bitcoin-stack-2', ['X']);
+    const stack3 = new BitcoinStack('bitcoin-stack-3', ['Item']);
+    const stack5 = new BitcoinStack('bitcoin-stack-5', ['Item']);
+    const stack6 = new BitcoinStack('bitcoin-stack-6', ['Item']);
 
-    // Bind the buttons to the specific stack instance
     document.getElementById('cat-button-1').onclick = () => stack1.concatenateTopTwo();
-
     document.getElementById('dup-button-2').onclick = () => stack2.duplicateTop();
-
     document.getElementById('dup-button-3').onclick = () => stack3.duplicateTop();
     document.getElementById('cat-button-3').onclick = () => stack3.concatenateTopTwo();
-
-    document.getElementById('dup-button-4').onclick = () => stack4.duplicateTop();
-    document.getElementById('cat-button-4').onclick = () => stack4.concatenateTopTwoLimit('5000', 'error-popup-5000');
-
     document.getElementById('dup-button-5').onclick = () => {
         stack5.duplicateTop();
     };
-    
     document.getElementById('cat-button-5').onclick = () => {
         stack5.concatenateTopTwoLimit('5000', 'error-popup-5000-2');
     };
-
     document.getElementById('dup-button-6').onclick = () => stack6.duplicateTop();
     document.getElementById('cat-button-6').onclick = () => stack6.concatenateTopTwoLimit('520', 'error-popup-520');
 });
@@ -175,23 +144,19 @@ document.addEventListener('DOMContentLoaded', function() {
 function startInstallation(event, popupId) {
     var upgradeButton = event.target;
     var popup = document.getElementById(popupId);
-    var elem = popup.querySelector('.installation-bar'); // Selects the installation bar within the popup
-    var width = 0; // Reset to 0 for a fresh start
+    var elem = popup.querySelector('.installation-bar');
+    var width = 0;
 
-    // Reset the width of the installation bar
     elem.style.width = '0%';
-
-    // Show the popup
     popup.style.display = 'block';
-
     var intervalId = setInterval(frame, 50);
 
     function frame() {
         if (width >= 100) {
             clearInterval(intervalId);
-            popup.style.display = 'none'; // Hide the popup
-            upgradeButton.textContent = 'Next >'; // Change button text to 'Next'
-            upgradeButton.onclick = nextWindow; // Change onclick event to nextWindow function
+            popup.style.display = 'none';
+            upgradeButton.textContent = 'Next >';
+            upgradeButton.onclick = nextWindow;
         } else {
             width++;
             elem.style.width = width + '%';
